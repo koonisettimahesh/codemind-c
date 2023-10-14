69c54cd0cbce1e0adbb8d9dc2992c7eca737b0c6
koonisettimahesh <147791718+koonisettimahesh@users.noreply.github.com>
2023-10-14 11:09:14 +0530

    //bitwise operators #include<stdio.h> int main() { 	char alp; 	scanf("%c",&alp); 	switch(alp) 	{ 	    case 'A': 		case 'a': 		case 'E': 	    case 'e': 		case 'I': 		case 'i':
    		case 'O':
    		case 'o':
    		       
    		case 'U':
    		case 'u' :printf("VOWEL");
    		       break;
    		       
    		default: printf("CONSONANT");
    		       
    	}
    
    }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,27 @@
+//bitwise operators
+#include<stdio.h>
+int main()
+{
+	char alp;
+	scanf("%c",&alp);
+	switch(alp)
+	{
+	    case 'A':
+		case 'a':
+		case 'E':
+	    case 'e':
+		case 'I':
+		case 'i':
+		       
+		case 'O':
+		case 'o':
+		       
+		case 'U':
+		case 'u' :printf("VOWEL");
+		       break;
+		       
+		default: printf("CONSONANT");
+		       
+	}
+
+}
