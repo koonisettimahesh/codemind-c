6c02367a0a8654588a1b5ec967f91a81ab44e074
koonisettimahesh <147791718+koonisettimahesh@users.noreply.github.com>
2023-11-24 12:10:35 +0530

    #include<stdio.h> int main() {     int i=1,n,r,s=0,p=1;     scanf("%d",&n);     while(n!=0)     {
                r=n%10;
                s=s+r;
                p=p*r;
                n=n/10;
                i++;
        }
        if(s==p)
        {
            printf("Spy Number");
            
        }
        else 
        {
            printf("Not Spy Number");
        }
    }

---
+++ Spy_Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i=1,n,r,s=0,p=1;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+
+            r=n%10;
+            s=s+r;
+            p=p*r;
+            n=n/10;
+            i++;
+    }
+    if(s==p)
+    {
+        printf("Spy Number");
+        
+    }
+    else 
+    {
+        printf("Not Spy Number");
+    }
+}
