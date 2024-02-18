4e57407990b0be9ea0a1ab208123b60f2b8d95c6
koonisettimahesh <147791718+koonisettimahesh@users.noreply.github.com>
2024-02-18 16:12:33 +0530

    #include<stdio.h> int main(){     char s[100];     scanf("%[^ ]",s);     int i,c=0;     for(i=0;s[i]!=NULL;i++)     {         if(s[i]>='a'&&s[i]<='z')         {             c++;         }     }printf("%d",c);
    }

---
+++ Lowercase_Letters_in_a_String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char s[100];
+    scanf("%[^
+]",s);
+    int i,c=0;
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]>='a'&&s[i]<='z')
+        {
+            c++;
+        }
+    }printf("%d",c);
+    
+}
