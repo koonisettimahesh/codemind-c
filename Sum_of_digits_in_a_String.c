69514f511b347e821880d4df546b5a1fd89d7588
koonisettimahesh <147791718+koonisettimahesh@users.noreply.github.com>
2024-02-18 18:26:51 +0530

    #include<stdio.h> int main(){     char s[100],ch='0';     scanf("%[^ ]",s);     int i,sum=0;     for(i=0;s[i]!=NULL;i++)     {         if(s[i]>='0'&&s[i]<='9')         {             sum=sum+s[i]-ch;         }     }     printf("%d",sum); }

---
+++ Sum_of_digits_in_a_String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char s[100],ch='0';
+    scanf("%[^
+]",s);
+    int i,sum=0;
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]>='0'&&s[i]<='9')
+        {
+            sum=sum+s[i]-ch;
+        }
+    }
+    printf("%d",sum);
+}
