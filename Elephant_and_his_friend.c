2affdd994a4172a70393a2c10460d8722c5bb9f3
koonisettimahesh <147791718+koonisettimahesh@users.noreply.github.com>
2024-03-19 23:15:21 +0530

    #include<stdio.h> int main(){     int n,c,r;     scanf("%d",&n);     r=n%5;     c=n/5;     if(r!=0)     {         printf("%d",c+1);     }     else     {         printf("%d",c);     } }

--- Elephant_and_his_friend.c
+++ Elephant_and_his_friend.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 int main(){
-    int n,c;
+    int n,c,r;
     scanf("%d",&n);
-    c=(n-1)/5;
-    printf("%d",c+1);
+    r=n%5;
+    c=n/5;
+    if(r!=0)
+    {
+        printf("%d",c+1);
+    }
+    else
+    {
+        printf("%d",c);
+    }
 }
